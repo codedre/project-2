fe5a7939829610fe6a33874549c7fa5b9dcec6a6
ANDRE SHONUBI <andre@codedre.com>
2015-11-18 13:29:30 -0500

    set up facebook integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root to: "home#index"
   get "search", to: "home#search"
 
   resources :moods do
     resources :foods
   end
+
+#   devise_scope :user do
+#   delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+# end
 end
