19828c6c0e331425cf8c51c1513ae66463114411
ANDRE SHONUBI <andre@codedre.com>
2015-11-19 01:30:35 -0500

    styles

--- db/migrate/20151117235741_create_foods.rb
+++ db/migrate/20151117235741_create_foods.rb
@@ -1,9 +1,9 @@
 class CreateFoods < ActiveRecord::Migration
   def change
     create_table :foods do |t|
-      t.string :type
+      t.string :name
       t.timestamps
-      t.belongs_to :moods
+      t.belongs_to :mood
     end
   end
 end
