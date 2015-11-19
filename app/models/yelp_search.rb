e2823855692e4fb6111a237109c1068e5f7662a1
CHRYSTAL BANKS <Chrystal.Banks1@gmail.com>
2015-11-19 16:47:20 -0500

    comments made on your project by CB

--- app/models/yelp_search.rb
+++ app/models/yelp_search.rb
@@ -1,6 +1,6 @@
 class YelpSearch
   attr_reader :businesses
-  def initialize(query, paramas)
+  def initialize(query, paramas) #params is misspelled.
     response = Yelp.client.search(query, paramas)
     @businesses = response.businesses
   end
